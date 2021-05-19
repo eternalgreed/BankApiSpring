8ffd7ab2a00848282b5b37a68ba268b5513bd526
Абдуллаев Рамазан Абдулкамалович <RAbAbdullaev@sberbank.ru>
2021-05-19 12:17:02 +0300

    Пропустил аннотацию Test

--- src/test/java/com/example/bankapi/repository/CardRepositoryImplTest.java
+++ src/test/java/com/example/bankapi/repository/CardRepositoryImplTest.java
@@ -1,22 +1,55 @@
 package com.example.bankapi.repository;
 
-import org.junit.Test;
+import com.example.bankapi.entity.Card;
+import com.example.bankapi.exception.NoSuchAccountException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-public class CardRepositoryImplTest {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+class CardRepositoryImplTest {
+    @Autowired
+    CardRepositoryImpl cardRepository;
 
     @Test
-    public void getAllCardsForAccount() {
+    public void getAllByAccountId() {
+        List<Card> cardsExpected = new ArrayList<>(Arrays.asList(
+                new Card(1, "1488322845651234", 1, false),
+                new Card(2, "3228148845671234", 1, false)));
+        List<Card> cardsActual = cardRepository.getAllByAccountId(1);
+        assertEquals(cardsExpected, cardsActual);
     }
 
     @Test
-    public void createNewCard() {
+    public void getAllByAccountIdNoSuchAccExc() {
+        assertThrows(NoSuchAccountException.class, () -> cardRepository.getAllByAccountId(6));
+    }
+
+
+    @Test
+    public void createByAccountIdSuccess() {
+        Card byAccountId = cardRepository.createByAccountId(1);
+        int idActual = byAccountId.getId();
+        assertEquals(3, idActual);
     }
 
     @Test
-    public void getBalance() {
+    public void crateByAccountIdNoSuchAccExc() {
+        assertThrows(NoSuchAccountException.class, () -> cardRepository.createByAccountId(6));
     }
 
-    @Test
-    public void increaseBalance() {
-    }
 }
